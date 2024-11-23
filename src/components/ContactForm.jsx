75e655740a0f2c5491275f1cda3305fbbc4ba525
Maria <mariafacchin00@hotmail.com>
2024-11-23 19:34:47 +0100

    style Contact form

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -1,22 +1,52 @@
 import React from "react";
+import "../styles/contactForm.css";
 
 export default function ContactForm() {
   return (
-    <>
-      <h1>Contact Me on:</h1>
-      <p>WhatsApp: 47 9 9678 0945</p>
-      <h2>OR</h2>
-      <form name="contact" method="POST" data-netlify="true">
-        {/* Hidden input for Netlify form processing */}
-        <input type="hidden" name="form-name" value="contact" />
-        <label htmlFor="name">Name:</label>
-        <input type="text" id="name" />
-        <label htmlFor="email">Email:</label>
-        <input type="email" id="email" />
-        <label htmlFor="message">Message:</label>
-        <textarea id="message"></textarea>
-        <button type="submit">Send</button>
-      </form>
-    </>
+    <div className="contact">
+      <div className="contact-info">
+        <h1>Contact</h1>
+        <p>WhatsApp: 47 9 9678 0945</p>
+        <h2>or</h2>
+      </div>
+
+      <div className="form">
+        <div>
+          <img src="/images/cleaning.jpeg" alt="" className="image" />
+        </div>
+
+        <form name="contact" method="POST" data-netlify="true">
+          {/* Hidden input for Netlify form processing */}
+          <input type="hidden" name="form-name" value="contact" />
+
+          <div className="form-items">
+            <input
+              type="text"
+              className="input-items"
+              placeholder="Name"
+              required
+            />
+          </div>
+          <div className="form-items">
+            <input
+              type="email"
+              className="input-items"
+              placeholder="Email"
+              required
+            />
+          </div>
+          <div className="form-items">
+            <textarea
+              className="input-items text-area"
+              placeholder="
+Optional message"
+            ></textarea>
+          </div>
+          <button type="submit" className="button">
+            Send
+          </button>
+        </form>
+      </div>
+    </div>
   );
 }
