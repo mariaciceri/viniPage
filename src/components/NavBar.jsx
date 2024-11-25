8991fc542b6873eb039a3970dd50367bc50c7ca0
Maria <mariafacchin00@hotmail.com>
2024-11-25 09:29:30 +0100

    Fix typo

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
-import "../styles/navBar.css";
+import "../styles/navbar.css";
 
 export default function NavBar() {
   return (
